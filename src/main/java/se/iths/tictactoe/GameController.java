17728a3b745f1134634f6fa2bd9b8581516892c5
altmannk <katherine.altmann@iths.se>
2023-10-19 17:20:11 +0200

    adding buttons, labels and event methods

--- src/main/java/se/iths/tictactoe/GameController.java
+++ src/main/java/se/iths/tictactoe/GameController.java
@@ -1,8 +1,38 @@
 package se.iths.tictactoe;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class GameController {
 
+    public Button resetScoreButton;
+    public Label playerScoreLabel;
+    public Label computerScoreLabel;
+    @FXML
+    private Button button1;
+    @FXML
+    private Button button2;
+    @FXML
+    private Button button3;
+    @FXML
+    private Button button4;
+    @FXML
+    private Button button5;
+    @FXML
+    private Button button6;
+    @FXML
+    private Button button7;
+    @FXML
+    private Button button8;
+    @FXML
+    private Button button9;
+
+
+    public void handleButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void handleResetScoreClick(ActionEvent actionEvent) {
+    }
 }
