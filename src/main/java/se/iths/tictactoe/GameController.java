03fa48c0963da95fd9825eb1e7a9829ccb855c00
Katherine <katherine.altmann@iths.se>
2023-10-27 15:42:05 +0200

    Changed current player to an int

--- src/main/java/se/iths/tictactoe/GameController.java
+++ src/main/java/se/iths/tictactoe/GameController.java
@@ -4,16 +4,18 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GameController {
     public GameModel model = new GameModel();
     public Button button1, button2, button3, button4, button5, button6, button7, button8, button9, resetScoreButton;
     public List<Button> buttons = new ArrayList<>();
-    private String currentPlayer;
+    private int currentPlayer;
 
     public void initialize() {
-        currentPlayer = "X";
+        currentPlayer = 0; // Index 0 corresponds to 'X', index 1 corresponds to '0'
+        buttons = Arrays.asList(button1, button2, button3, button4, button5, button6, button7, button8, button9);
     }
 
     public GameModel getModel() {
@@ -23,11 +25,11 @@ public class GameController {
     public void handleButtonClick(ActionEvent actionEvent) {
         Button clickedButton = (Button) actionEvent.getSource();
 
-        if(clickedButton.getText().isEmpty()) {
-            clickedButton.setText(currentPlayer);
-            clickedButton.setDisable(true);
-            currentPlayer = (currentPlayer.equals("X") ? "O" : "X");
-        }
+        clickedButton.setText((currentPlayer == 0 ? "X" : "O")); // update button text w current player
+        clickedButton.setDisable(true);
+        currentPlayer = (currentPlayer + 1) % 2; //switch player
+
+
     }
 
     public void handleResetScoreClick(ActionEvent actionEvent) {
